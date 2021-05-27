1b53372930bbeb12af150b8b3fbb0a8f99f5ef4a
n-yushin <gaiabass0215nomura@gmail.com>
2021-05-27 20:24:37 +0900

    5/25分コミット

--- myPortfolio1/js/myportfoliosite.js
+++ myPortfolio1/js/myportfoliosite.js
@@ -1,8 +1,23 @@
 $(function(){
 
+  // スマホ用画面上部ハンバーガーメニュー
+  /* $('.Toggle').click (function() {
+    $(this).toggleClass('active');
+    $('page_nav_small').toggleClass('open');
+  }); */
+
+  $('.Toggle').click (function() {
+    $(this).toggleClass('active');
+    // $('.page_nav_small').toggleClass('open');
+    $('.page_nav_small').slideToggle();
+  });
+
+  // woksにおける職歴タイトルのクリックイベント
   $('.js_title_link').each(function(){
       $(this).on('click',function(){
+          // 指定箇所押下で開閉
           $("+.job_sub",this).slideToggle();
+          // アコーディオンメニュー開閉における+=切り替え
           if ($(this).children('span').text() == "＋") {
             $(this).children('span').text('－');
           }
